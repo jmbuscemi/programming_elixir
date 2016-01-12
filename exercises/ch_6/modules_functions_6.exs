bcdc9e85c2aa2455855ac82b4a8d748536fa08bd
John M Buscemi <jmbuscemi@gmail.com>
2016-01-11 19:05:36 -0500

    Chapter 6 - Exercise 6

--- exercises/ch_6/modules_functions_6.exs
+++ exercises/ch_6/modules_functions_6.exs
@@ -1,17 +1,25 @@
 #Binary search number guessing.
 
 defmodule Chop do
-  def guess(actual, a..b) when actual < a or actual > b do
-    IO.puts "You must guess a number between #{a} and #{b}"
+  def guess(actual, low..high) when actual < low or actual > high do
+    IO.puts "You must guess a number between #{low} and #{high}"
   end
 
-  def guess(actual, a..b) when actual in a..b do
-    computer_guess = div((a + b), 2)
+  def guess(actual, low..high) when actual in low..high do
+    computer_guess = div(low + high, 2)
     IO.puts "Is it #{computer_guess}?"
-    next_guess(actual, a..b, computer_guess)
+    _check(actual, computer_guess, low..high)
   end
 
-  defp next_guess(actual, a..b, computer_guess) when actual == computer_guess do
+  defp _check(actual, computer_guess, _) when actual == computer_guess do
     actual
+  end
+
+  defp _check(actual, computer_guess, _..high) when computer_guess < actual do
+    guess(actual, (computer_guess+1)..high)
+  end
+
+  defp _check(actual, computer_guess, low.._) when computer_guess > actual do
+    guess(actual, low..(computer_guess-1))
   end
 end
